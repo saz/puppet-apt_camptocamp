fd40ebc36571163584ae2e3b42b53ddcc48cee15
Saz <me@saz.sh>
2011-11-21 23:58:40 +0100

    Run apt-key as root user, fixes #1

--- manifests/key.pp
+++ manifests/key.pp
@@ -20,6 +20,7 @@ define apt::key($ensure=present, $source="", $content="") {
         command => "${thekey} | apt-key add -",
         unless => "apt-key list | grep -Fqe '${name}'",
         path   => "/bin:/usr/bin",
+        user => root,
         before => Exec["apt-get_update"],
         notify => Exec["apt-get_update"],
       }
@@ -28,6 +29,7 @@ define apt::key($ensure=present, $source="", $content="") {
     absent: {
       exec {"apt-key del ${name}":
         onlyif => "apt-key list | grep -Fqe '${name}'",
+        user => root,
       }
     }
 
